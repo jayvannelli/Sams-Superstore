a55e7ac42056a67c413bac954984f6134eb4b3cc
jayvannelli <jaymvannelli@gmail.com>
2023-03-14 09:44:47 -0400

    Update main page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,10 @@ from streamlit_extras.dataframe_explorer import dataframe_explorer
 
 def main():
     st.set_page_config(page_title="Streamlit Superstore Dashboard",
-                       page_icon=":shopping_trolley:")
+                       page_icon="🛒",
+                       layout="wide")
 
-    st.title("Sam's Superstore | Sales Dashboard")
-    st.write("""
-        Hello! This application is built using data from Kaggle 
-        (link to listing & dataset below) and showcases some of 
-        the many things possible with streamlit.
-    """)
+    st.title("Sam's Superstore | Sales Dashboard | Streamlit | Kaggle")
     st.image("images/acsi-supermarket-industry-scaled.jpg")
 
     st.subheader("Link to full Kaggle listing & dataset")
